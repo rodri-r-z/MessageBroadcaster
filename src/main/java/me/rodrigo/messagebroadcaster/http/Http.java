accab93168b4a712781b6b33d5559da292253d0c
Rodrigo Ramirez <141367232+rodri-r-z@users.noreply.github.com>
2023-12-08 16:19:11 -0500

    upload fix

--- src/main/java/me/rodrigo/messagebroadcaster/http/Http.java
+++ src/main/java/me/rodrigo/messagebroadcaster/http/Http.java
@@ -5,13 +5,14 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class Http {
     public static void DownloadFile(String uri, String path) throws IOException {
         URL url = new URL(uri);
         InputStream inputStream = url.openStream();
-        Path savePath = Path.of(path, url.getFile());
+        Path savePath = Paths.get(path);
 
         Files.copy(inputStream, savePath, StandardCopyOption.REPLACE_EXISTING);
     }
